79360972ab4f9cfebe9a2c443f8c13724d2e1297
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-04-28 18:24:54 -0400

    Update readticker.js

--- readticker.js
+++ readticker.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var qs = require('querystring');
+var port = process.env.PORT || 3000;
 const csv = require('csv-parser');
 const MongoClient = require('mongodb').MongoClient;
 const urll = "mongodb+srv://unellu01:aaa@cluster0.xd9qb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
